036048c2a83823c08255536ca08c657cc2f2a5b2
Jay <jiyeyoun@outlook.com>
2026-01-10 10:57:31 +0900

    fix: Include CSS files for the library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import "./css/dropdown.css";
+import "./css/editor.css";
+import "./css/preview.css";
+import "./css/toolbar.css";
+
 export * from "./components";
 export * from "./constants";
 export * from "./hooks";

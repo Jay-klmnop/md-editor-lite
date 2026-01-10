ebae893536f4d9a1d35cee9290894775958d7937
Jay <jiyeyoun@outlook.com>
2026-01-10 12:15:12 +0900

    fix: Add external: ["react", "react-dom"] to tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/style.css"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   clean: true,
   loader: {
     ".css": "copy",
   },
+  external: ["react", "react-dom"],
   esbuildOptions(options) {
     options.jsx = "automatic";
   },

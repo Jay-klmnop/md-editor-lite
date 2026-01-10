c81cd4072447e7209a35f0b6cacb0cd374430318
Jay <jiyeyoun@outlook.com>
2026-01-10 11:21:51 +0900

    build: Expose css entry through exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: {
+    index: "src/index.ts",
+    style: "src/style.css",
+  },
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
